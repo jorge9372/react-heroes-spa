56d6221b331dd7a24529f640dd72ac3912e21e1c
Jorge Villafraz <jorge9372@gmail.com>
2024-04-20 23:19:13 -0400

    feat: login with localStorage

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -2,12 +2,44 @@ import { useReducer } from 'react';
 import { AuthContext } from './AuthContext';
 import { authReducer } from './authReducer';
 
-const initialState = {
-    logged: false,
+import { types } from '../types/types';
+
+const init = () => {
+    const user = JSON.parse(localStorage.getItem('user'));
+
+    return {
+        logged: !!user,
+        user,
+    };
 };
 
 export const AuthProvider = ({ children }) => {
-    const [state, dispatch] = useReducer(authReducer, initialState);
+    const [authState, dispatch] = useReducer(authReducer, {}, init);
 
-    return <AuthContext.Provider value={{}}>{children}</AuthContext.Provider>;
+    const login = (name = '') => {
+        const user = {
+            id: 'ABC',
+            name,
+        };
+
+        const action = {
+            type: types.login,
+            payload: user,
+        };
+
+        localStorage.setItem('user', JSON.stringify(user));
+
+        dispatch(action);
+    };
+
+    return (
+        <AuthContext.Provider
+            value={{
+                ...authState,
+                login: login,
+            }}
+        >
+            {children}
+        </AuthContext.Provider>
+    );
 };
